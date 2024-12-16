45e25ae335ff8bc545c6549d0fc18b512958cbb3
Victor Magueta <56882461+vmagueta@users.noreply.github.com>
2024-12-16 12:45:35 +0000

    Readme (#3)
    * Organizado as pastas e iniciado o projeto com Django
    
    * Initialized the blog project
    
    * Updated README with project details and instructions

--- kamila_project/app/models.py
+++ kamila_project/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    slug = models.SlugField(unique=True, max_length=200)
+    content = models.TextField(verbose_name="Conteúdo")
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Data de Criação"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Última atualização"
+    )
+
+    def __str__(self):
+        return self.title
+
+    def save(save, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
