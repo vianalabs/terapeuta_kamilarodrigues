7ecb5682c6a6a5ef83ba138e1cf24ee4facdafe7
Victor Magueta <56882461+vmagueta@users.noreply.github.com>
2025-01-06 16:07:26 +0000

    Setup (#9)
    * reorganized the folders, created the initial setup, and created the base html
    
    * added the make live command
    
    * fixed missing the command in PHONY
    
    * Created pipeline for CI, fixed the requirements of the project and make a new test for testing CI
    
    * Created pipeline for CI, fixed the requirements of the project and make a new test for testing CI
    
    * Fixed the error of requirements.test.txt and removed livereload untill find a way to use it with tests
    
    * Fixed the error of missing pflake on requirements
    
    * Fixed the test, removed --forked from test
    
    * Update views.py

--- kamila_project/settings.py
+++ kamila_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "app",
-    "livereload",
+    # "livereload",
 ]
 
 MIDDLEWARE = [
@@ -51,28 +51,26 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "livereload.middleware.LiveReloadScript",
+    # "livereload.middleware.LiveReloadScript",
 ]
 
 ROOT_URLCONF = "kamila_project.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
-
-
 
 
 WSGI_APPLICATION = "kamila_project.wsgi.application"
